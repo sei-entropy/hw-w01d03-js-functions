9202fb015611f3841942035cd2bedba4ef19766b
Robaarishi24 <59921337+Robaarishi24@users.noreply.github.com>
2020-01-29 04:25:11 +0300

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,54 +1,36 @@
-const playGame= function(userChoice)
-{
-    let computerChoice=Math.floor((Math.random() * 3) + 1);
-
-    if(userChoice==="rock")
-    {
-        
-     if(computerChoice===1 ) 
-     {
-         return("user tie")
-        };
+const playGame = function(userChoice){
     
-     if(computerChoice===2 ) 
-     {
-         return("user Lose")
-        };
+    let computerChoice = Math.floor(Math.random()*3) + 1;
 
-     if(computerChoice===3 ) 
-     {
-         return("User win")
-        }
-    };
-    
-    if(userChoice==="paper")
-    {
-     if(computerChoice===1 ) 
-     {
-         return("user win")
-        };
-     if(computerChoice===2 )
-      {
-          return("user tie")
-        };
-     if(computerChoice===3 ) 
-     {
-         return("user lose")
-    };
+    if(userChoice === 'rock' && computerChoice === 1){
+        return 'It is a tie!';
     }
- if(userChoice==="scissors")
- {
-     if(computerChoice===1 ) 
-     {
-         return("user lose")
-        };
-     if(computerChoice===2 ) 
-     {
-         return("user win")
-        };
-     if(computerChoice===3 )
-      {
-         return("user tie")
-        };
+    else if(userChoice === 'rock' && computerChoice === 2){
+        return 'User lose.';
     }
-}
+    else if(userChoice === 'rock' && computerChoice === 3){
+        return 'User win!';
+    }
+    else if(userChoice === 'paper' && computerChoice === 1){
+        return 'User win!';
+    }
+    else if(userChoice === 'paper' && computerChoice === 2){
+        return 'It is a tie!';
+    }
+    else if(userChoice === 'paper' && computerChoice === 3){
+        return 'User lose.';
+    }
+    else if(userChoice === 'scissors' && computerChoice === 1){
+        return 'User lose.';
+    }
+    else if(userChoice === 'scissors' && computerChoice === 2){
+        return 'User win !';
+    }
+    else if(userChoice === 'scissors' && computerChoice === 3){
+        return 'It is a tie!';
+    }
+    else {
+        return 'Please enter a valid input.';
+    }
+} 
+playGame('rock')
