cd8b21bb3f008e57f85e95ad3e2ebc11d5d6a033
Robaarishi24 <59921337+Robaarishi24@users.noreply.github.com>
2020-01-29 04:29:59 +0300

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,13 +1,8 @@
-const rollDice= function(num=2)
-{
-    let sum=0;
-    let number; 
-    for(let i=1;i<num;i++)
-    {
-    number=Math.floor((Math.random() * 6)+1);
-    sum= sum+number;
-    }
-    number=Math.floor((Math.random() * 6)+1);
-    sum+=number;
-    return console.log ('Dice rolled are '+number+' and '+number+'. Sum is '+sum)
-}; 
+const rollDice = function(){
+    let num1 = Math.floor(Math.random() * 6) + 1
+    let num2 = Math.floor(Math.random() * 6) + 1
+    let sum = num1 + num2;
+  
+    return 'Dice rolled are ' + num1 + ' and ' + num2 + '. Sum is ' + sum ;
+}
+rollDice() 
