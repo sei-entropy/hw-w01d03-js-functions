e7ae03d67ba775820e34c6d9ac7f70aca28176f2
wejdann <weejdansaud58@hotmail.com>
2020-02-06 16:56:58 +0300

    HW function

--- dice.js
+++ dice.js
@@ -1,19 +1,8 @@
-/*function rollDie(){
+function rollDie() {
 
-  var num= Math.floor((Math.random() * 6) + 0); 
-for (var i=0 ; i <6 ; i++) {
-var total = 0;
-total += num;
-return total  ; } } */
-function rollDie(){
+  const num1 = Math.floor((Math.random() * 6) + 1);
+  const num2 = Math.floor((Math.random() * 6) + 1);
 
-  var num1= Math.floor((Math.random() * 6) + 1); 
-  var num2= Math.floor((Math.random() * 6) + 1); 
-for (var i=0 ; i <6 ; i++) {
-var total = 0;
-total += num1+num2;
-console.log(' Dice rolled are' +'  ' + num1 + ' , '+ num2 +'. Sum is '+total); ; } 
-
+  const total = num1 + num2;
+  return ('Dice rolled are' + '  ' + num1 + ' , ' + num2 + '. Sum is ' + total);
 }
- 
-  
--- rps.js
+++ rps.js
@@ -1,16 +1,17 @@
-const thelistOfchoice=['rock','scissors','paper'];
+const theChoices = ['rock', 'scissors', 'paper'];
+
 function playGame(userChoice) {
-var computerChoice =Math.floor(Math.random()*3);
-computerChoice=thelistOfchoice[computerChoice]
+
+  var computerChoice = Math.floor(Math.random() * 3);
+
+  computerChoice = theChoices[computerChoice]
+  console.log("computer picked :" + computerChoice + ". " + " " + "User picked : " + userChoice + ". ");
+
   if (userChoice === computerChoice) {
-    return 'The game is a tie!';
-  } 
-    
-  else if (userChoice === 'rock' || 'paper' || 'scissors') {
-    if (computerChoice === 'rock') {
-       return 'You won!';
-    } else {
-       return 'your lose!';
-    } 
-   }
+    console.log("Tie");
+  } else if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'scissors' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'rock')) {
+    console.log("win");
+  } else {
+    console.log("Lose");
   }
+}
